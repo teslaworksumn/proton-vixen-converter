cc919b5f034f71a7d641e502233d758e5390ca8c
Light Show Admin <cselightshow@gmail.com>
2016-12-09 16:16:13 -0600

    Added preshow and midshow to import-sequences

--- vixenconverter/import-sequences.sh
+++ vixenconverter/import-sequences.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # import-sequences.py <proton-path> <admin-key> <layout-id>
 
 set -o nounset
 set -o errexit
 
-dir="$HOME/Dropbox/Sequencing 2016/Sequencing/Sequence Data/Sequences"
+dir="$HOME/Dropbox/Sequencing 2016/Sequencing/Sequence Data"
 seq_dir="$dir/Sequences"
 audio_dir="$dir/Audio"
 
@@ -15,8 +15,10 @@ seqs=( \
   "(1) Jingle Breaks.vix" \
   "(2) Feel Good.vix" \
   "(3) Collide.vix" \
-  "(4) Unicorn Adventure_Master.vix" \
+  "(4) Unicorn Adventure.vix" \
   "(5) Glory to the Bells.vix" \
+  "Preshow.vix" \
+  "Midshow.vix" \
 )
 
 audio=( \
@@ -25,8 +27,10 @@ audio=( \
   "(3) Collide.ogg" \
   "(4) Unicorn Adventure_Master.ogg" \
   "(5) Glory to the Bells.ogg" \
+  "Preshow.ogg" \
+  "Midshow.ogg" \
 )
 
-for i in {0..4}; do
-  echo ./converter.py "$1" "$2" "$seq_dir/${seqs[$i]}" "$audio_dir/${audio[$i]}" "$3"
+for i in {0..6}; do
+  ./converter.py import-sequence "$1" "$2" "$seq_dir/${seqs[$i]}" "$audio_dir/${audio[$i]}" "$3"
 done
